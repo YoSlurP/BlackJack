5e378526654f7a0e6adf9c2efdc3c6beab6ccc98
YoSlurP <cseh.lalika.gergo@gmail.com>
2024-04-17 21:39:21 +0200

    Igten

--- src/main/java/com/example/blackjack/HelloController.java
+++ src/main/java/com/example/blackjack/HelloController.java
@@ -2,11 +2,14 @@ package com.example.blackjack;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 import java.net.*;
-import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
 
 public class HelloController {
 
@@ -16,11 +19,19 @@ public class HelloController {
     @FXML private TextField belepes;
     @FXML private TextField server;
     @FXML private Label ertek;
+    @FXML private Label tet;
+    @FXML private VBox bc;
+    @FXML private Button g;
     public int penz=0;
     public int bet=0;
+    public LinkedList<String >kartyak= new LinkedList<>();
+    public LinkedList<String >oszto= new LinkedList<>();
+    public int n=580,o=514;
+    public int l=0;
 
 
     public void initialize(){
+        bc.setStyle("-fx-background-color: linear-gradient(to right bottom, #015294, #02f1f5);");
         try {
             socket=new DatagramSocket();
         } catch (Exception e) {
@@ -68,17 +79,56 @@ public class HelloController {
         String[] s=uzenet.split(":");
         if(s[0].equals("joined")){
             ertek.setText(s[1]+" Ft");
-        }else if(s[0].equals("start")){
-
-        }else if(s[0].equals("paid")){
-
-        }else if(s[0].equals("k")){
+        }
+        if(s[0].equals("start")){
+            g.setDisable(true);
+        }
+        if(s[0].equals("paid")){
+            ertek.setText(s[1]+" Ft");
+        }
+        if(s[0].equals("s")){
+            ImageView asd=new ImageView(s[1].charAt(0) + s[1].charAt(1) + ".png");
+            ImageView as=new ImageView("card back black.png");
+            as.setLayoutX(649);as.setLayoutY(209);as.setFitHeight(80);as.setFitWidth(70);
+            asd.setLayoutX(532);asd.setLayoutY(209);asd.setFitHeight(80);asd.setFitWidth(70);
+            oszto.add(s[1].charAt(0) + s[1].charAt(1) + ".png");
+        }
+        if(s[0].equals("k")){
+            ImageView a=new ImageView(s[1].charAt(0) + s[1].charAt(1) + ".png");
+            a.setLayoutX(n);a.setLayoutY(o);
+            a.setFitHeight(80);a.setFitWidth(70);
+            kartyak.add(s[1].charAt(0) + s[1].charAt(1) + ".png");
+        }
+        if (s[0].equals("k") || kartyak.size()<2 ){
+            ImageView a=new ImageView(s[1].charAt(0) + s[1].charAt(1) + ".png");
+            a.setLayoutX(n+30);a.setLayoutY(o+30);n=n+30;o=o+30;
+            a.setFitHeight(80);a.setFitWidth(70);
+            kartyak.add(s[1].charAt(0) + s[1].charAt(1) + ".png");
+        }
+        if (s[0].equals("k") || kartyak.size()>2 ){
+            ImageView a=new ImageView(s[1].charAt(0) + s[1].charAt(1) + ".png");
+            a.setLayoutX(n+30);a.setLayoutY(o+30);n=o+30;o=o+30;
+            a.setFitHeight(80);a.setFitWidth(70);
+            kartyak.add(s[1].charAt(0) + s[1].charAt(1) + ".png");
+        }
+        if(s[0].equals("s")|| oszto.size()>2){
+            ImageView k =new ImageView(s[1].charAt(0) + s[1].charAt(1) + ".png");
+            k.setLayoutX(649);
+            k.setLayoutY(209);
+            k.setFitHeight(80);
+            k.setFitWidth(70);
+        }
+        if(s[0].equals("end")){
+            n=580;o=514;
+        }
+        if(s[0].equals("balance")){
 
         }
+
     }
-
-
-
+    public void onResetClick(){
+        tet.setText("0 Ft");
+    }
     public void onHitclick(){
         kuld("hit",server.getText(),678);
     }
@@ -86,19 +136,28 @@ public class HelloController {
         kuld("stand",server.getText(),678);
     }
 
+    public void sendTet(){
+        kuld("bet:"+bet,server.getText(),678);
+
+    }
+    public void onKilepclick(){
+        kuld("exit",server.getText(),678);
+    }
+
     public void on100click(){
         bet+=100;
+        tet.setText(bet+" Ft");
     }
     public void onEgyclick(){
         bet+=1;
-
+        tet.setText(bet+" Ft");
     }
     public void on25click(){
         bet+=25;
-
+        tet.setText(bet+" Ft");
     }
     public void on50click(){
         bet+=50;
-
+        tet.setText(bet+" Ft");
     }
 }
